26cb0065c3538bd74cf68ab629db6b7ab6b341e9
TukuNoni71 <Adonisjzepeda@gmail.com>
2024-09-18 11:56:05 -0400

    finished conditional overview

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 const firstname = 'JD';
-let age = 44; 
+let age = 12; 
 
-age = age +1;
-console.log(age);
-
-console.log('tets');
+// If age is greater than 20 and less than 50, console log 'you are young'
+if (age > 20 && age < 50) {
+    console.log('you are young');
+}
+// If age is greater than or equal to 50, console log 'you are getting older'
+else if (age >= 50) {
+    console.log('you are getting older');
+}
+// If age is less 20 and greater than 12, console log 'you are a teenager'
+else if (age < 20 && age > 12 ) {
+    console.log('you are a teenager');
+}
+else {
+    console.log('your age is not within our range');
+}
